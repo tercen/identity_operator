cf22d69a3531f157b929b23402f4d7437cf0624d
ginberg <info@gerinberg.com>
2021-06-10 08:26:30 +0000

    simplify operator

--- main.R
+++ main.R
@@ -1,19 +1,11 @@
 library(tercen)
 library(dplyr)
 
-identity <- function(df, pair_length) {
-  pair1 <- df$.y[1:pair_length]
-  pair2 <- df$.y[(2*pair_length+1):(3*pair_length)]
-  df %>% mutate(identity = c(pair1, pair1, pair2, pair2))
-}
-
 ctx = tercenCtx()
 
-pair_length <- ifelse(is.null(ctx$op.value('Pair length')), 3, as.integer(ctx$op.value('Pair length')))
-
-ctx %>% 
-  select(.ci, .ri, .y) %>%
-  group_by(.ri) %>%
-  do(identity(., pair_length)) %>%
+ctx  %>% 
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(identity = .y, .groups = "drop") %>%
   ctx$addNamespace() %>%
   ctx$save()
